d2729949f895d69a860201ac0696f8468b0eca57
Matin Ansaripour <matinansaripour@gmail.com>
2024-04-07 13:52:19 +0200

    simple estimator

--- optimizers.py
+++ optimizers.py
@@ -29,7 +29,7 @@ class SGD(torch.optim.SGD):
 class ZAD(Optimizer):
     name = 'ZAD'
 
-    def __init__(self, params, lr=0.001, random_vec=100, momentum=0.9):
+    def __init__(self, params, lr=0.001, random_vec=100, momentum=0.9, names=None):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         defaults = dict(lr=lr, random_vec=random_vec, momentum=momentum)
         super(ZAD, self).__init__(params, defaults)
@@ -38,6 +38,12 @@ class ZAD(Optimizer):
         self.f = None
         self.momentum = momentum
         self.grad = [torch.zeros(p.size()).to(self.device) for group in self.param_groups for p in group['params']]
+        self.params = [p for group in self.param_groups for p in group['params']]
+        self.params_data = [p.data for p in params]
+        self.names = names
+        self.params_dict = {name: p for name, p in zip(self.names, self.params)}
+
+
 
     def set_f(self, model, data, target, criterion):
         names = list(n for n, _ in model.named_parameters())
@@ -53,7 +59,7 @@ class ZAD(Optimizer):
         # self.set_f(model, data, target, criterion)
         # params = [p for group in self.param_groups for p in group['params']]
         # params_data = [p.data for p in params]
-        total_loss = 0.0
+        # total_loss = 0.0
         # torch._foreach_mul_(self.grad, self.momentum)
         # for _ in range(self.random_vec):
         #     v = [torch.rand(p.size()).to(self.device) for p in params_data]
@@ -65,23 +71,44 @@ class ZAD(Optimizer):
         #
         # torch._foreach_add_(params_data, torch._foreach_mul(self.grad, -self.lr))
         # return total_loss / self.random_vec
-        params = {name: p for name, p in model.named_parameters()}
-        torch._foreach_mul_(self.grad, self.momentum)
+
+        # params = {name: p for name, p in model.named_parameters()}
+        # torch._foreach_mul_(self.grad, self.momentum)
+        # total_loss = 0.0
+        # for _ in range(self.random_vec):
+        #     tangents = {name: torch.clip(torch.rand_like(p), min=1e-3) for name, p in params.items()}
+        #     v = [t for t in tangents.values()]
+        #
+        #     dual_params = {}
+        #     with fwAD.dual_level():
+        #         for name, p in params.items():
+        #             dual_params[name] = fwAD.make_dual(p, tangents[name])
+        #         loss = criterion(functional_call(model, dual_params, data), target)
+        #         jvp_result = fwAD.unpack_dual(loss).tangent
+        #     torch._foreach_mul_(v, jvp_result.item() * (1 - self.momentum) / self.random_vec)
+        #     torch._foreach_add_(self.grad, v)
+        #     total_loss += loss.item()
+        #
+        # params_data = [p.data for p in params.values()]
+        # torch._foreach_add_(params_data, torch._foreach_mul(self.grad, -self.lr))
+        # return total_loss
+
+
         total_loss = 0.0
+        torch._foreach_mul_(self.grad, self.momentum)
         for _ in range(self.random_vec):
-            tangents = {name: torch.clip(torch.rand_like(p), min=1e-3) for name, p in params.items()}
-            v = [t for t in tangents.values()]
+            v = [torch.randn(p.size()).to(self.device) for p in self.params_data]
+            params_v = copy.deepcopy(self.params_dict)
+            for p, v_ in zip(params_v.items(), v):
+                p[1].data += v_
 
-            dual_params = {}
-            with fwAD.dual_level():
-                for name, p in params.items():
-                    dual_params[name] = fwAD.make_dual(p, tangents[name])
-                loss = criterion(functional_call(model, dual_params, data), target)
-                jvp_result = fwAD.unpack_dual(loss).tangent
-            torch._foreach_mul_(v, jvp_result.item() * (1 - self.momentum) / self.random_vec)
+            lossv = criterion(functional_call(model, params_v, data), target).item()
+            loss = criterion(functional_call(model, self.params_dict, data), target).item()
+
+            total_loss += loss
+            torch._foreach_mul_(v, (1 - self.momentum) * (lossv - loss) / self.random_vec)
             torch._foreach_add_(self.grad, v)
-            total_loss += loss.item()
 
-        params_data = [p.data for p in params.values()]
-        torch._foreach_add_(params_data, torch._foreach_mul(self.grad, -self.lr))
-        return total_loss
+        torch._foreach_add_(self.params_data, torch._foreach_mul(self.grad, -self.lr))
+        return total_loss / self.random_vec
+
