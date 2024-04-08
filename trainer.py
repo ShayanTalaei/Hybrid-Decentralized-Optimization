6fcf6f9ff25b9731760ac15345caae26dd4ad4f4
Matin Ansaripour <matinansaripour@gmail.com>
2024-04-08 17:01:53 +0200

    debug

--- trainer.py
+++ trainer.py
@@ -30,7 +30,6 @@ class HybridSGDTrainer:
         self.model.load_state_dict(initial_state_dict)
         self.test_loader = test_loader
         self.train_loader = train_loader
-        self.train_iterator = iter(train_loader)
         assert grad_mode in ['first_order', 'zeroth_order_forward-mode_AD', 'zeroth_order_simple']
         self.grad_mode = grad_mode
         self.criterion = get_criterion(dataset_name)
@@ -68,21 +67,16 @@ class HybridSGDTrainer:
                                      self.model_copy.nelement() * self.model_copy.element_size())
         self.win = MPI.Win.Create(buf, comm=self.comm)
 
-    def take_step(self):
+    def take_step(self, data, target):
         steps = 1
         taken_steps = 0
         total_loss = 0
         while taken_steps < steps:
-            try:
-                print("Rank: ", self.rank, 'optimize')
-                Xb, yb = next(self.train_iterator)
-                Xb, yb = Xb.to(self.model.device), yb.to(self.model.device)
-                loss = self.optimizer.optimize(self.model, Xb, yb, self.criterion)
-                taken_steps += 1
-                total_loss += loss
-            except StopIteration:
-                taken_steps += 1
-                iterator = iter(self.train_loader)
+            print("Rank: ", self.rank, 'optimize')
+            data, target = data.to(self.model.device), target.to(self.model.device)
+            loss = self.optimizer.optimize(self.model, data, target, self.criterion)
+            taken_steps += 1
+            total_loss += loss
         with self.warmup_scheduler.dampening():
             if self.warmup_scheduler.last_step + 1 >= self.scheduler_warmup_steps:
                 self.scheduler.step()
@@ -94,56 +88,59 @@ class HybridSGDTrainer:
             return self.train_solo()
 
         for taken_steps in range(self.total_step_number + self.warmup_steps):
+            step_loss = 0
             if self.steps % self.log_period == 0:
                 print(f"Rank {self.rank} steps: {self.steps}")
                 self.comm.Barrier()
                 self.evaluate()
                 self.comm.Barrier()
+            for (data, target) in self.train_loader:
 
-            # print(f"Rank {self.rank} steps: {self.steps} before take step")
+                # print(f"Rank {self.rank} steps: {self.steps} before take step")
 
-            loss = self.take_step()
-            # print(f"Rank {self.rank} steps: {self.steps} after take step")
-            if loss > 10 ** 4:  # Diverged!
-                return self.history
-            print(f"Rank {self.rank} steps: {self.steps}, loss: {loss}")
-            self.training_loss = self.training_loss * 0.95 + loss * 0.05 if self.training_loss is not None else loss
-            if self.steps < self.warmup_steps:
-                self.steps += 1
-                continue
+                loss = self.take_step(data, target)
+                step_loss += loss
+                # print(f"Rank {self.rank} steps: {self.steps} after take step")
+                if loss > 10 ** 4:  # Diverged!
+                    return self.history
+                if self.steps < self.warmup_steps:
+                    step_loss /= len(self.train_loader)
+                    self.training_loss = self.training_loss * 0.95 + step_loss * 0.05 if self.training_loss is not None else step_loss
+                    continue
 
-            # print(f"Rank {self.rank} steps: {self.steps} before lock")
+                # print(f"Rank {self.rank} steps: {self.steps} before lock")
 
-            self.win.Lock(self.rank, lock_type=MPI.LOCK_EXCLUSIVE)
-            # print(f"Rank {self.rank} steps: {self.steps} after lock")
+                self.win.Lock(self.rank, lock_type=MPI.LOCK_EXCLUSIVE)
+                # print(f"Rank {self.rank} steps: {self.steps} after lock")
 
-            self.model_to_copy(self.model_copy)
-            # print(f"Rank {self.rank} steps: {self.steps} after model to copy")
+                self.model_to_copy(self.model_copy)
+                # print(f"Rank {self.rank} steps: {self.steps} after model to copy")
 
-            self.win.Unlock(self.rank)
-            # print(f"Rank {self.rank} steps: {self.steps} after unlock")
+                self.win.Unlock(self.rank)
+                # print(f"Rank {self.rank} steps: {self.steps} after unlock")
 
-            partner_rank = np.random.randint(self.size)
-            while partner_rank == self.rank:
                 partner_rank = np.random.randint(self.size)
+                while partner_rank == self.rank:
+                    partner_rank = np.random.randint(self.size)
 
-            # print(f"Rank {self.rank} steps: {self.steps} before lock partner")
+                # print(f"Rank {self.rank} steps: {self.steps} before lock partner")
 
-            self.win.Lock(partner_rank, lock_type=MPI.LOCK_SHARED)
-            # print(f"Rank {self.rank} steps: {self.steps} after lock partner")
+                self.win.Lock(partner_rank, lock_type=MPI.LOCK_SHARED)
+                # print(f"Rank {self.rank} steps: {self.steps} after lock partner")
 
-            self.win.Get((self.partner_buf, MPI.FLOAT), target_rank=partner_rank)
+                self.win.Get((self.partner_buf, MPI.FLOAT), target_rank=partner_rank)
 
-            # print(f"Rank {self.rank} steps: {self.steps} after get")
+                # print(f"Rank {self.rank} steps: {self.steps} after get")
 
-            self.win.Unlock(partner_rank)
-            # print(f"Rank {self.rank} steps: {self.steps} after unlock partner")
+                self.win.Unlock(partner_rank)
+                # print(f"Rank {self.rank} steps: {self.steps} after unlock partner")
 
-            self.partner_model[:] = (self.partner_model + self.model_copy) / 2 if any(self.partner_model) else self.model_copy
+                self.partner_model[:] = (self.partner_model + self.model_copy) / 2 if any(self.partner_model) else self.model_copy
 
-            self.copy_to_model(self.partner_model)
-            # print(f"Rank {self.rank} steps: {self.steps} after copy to model")
-
+                self.copy_to_model(self.partner_model)
+                # print(f"Rank {self.rank} steps: {self.steps} after copy to model")
+            step_loss /= len(self.train_loader)
+            self.training_loss = self.training_loss * 0.95 + step_loss * 0.05 if self.training_loss is not None else step_loss
             self.steps += 1
         return self.history
 
@@ -152,11 +149,15 @@ class HybridSGDTrainer:
         for step in range(self.total_step_number):
             if self.steps % self.log_period == 0:
                 self.evaluate()
-
-            loss = self.take_step()
-            if loss > 10 ** 4:  # Diverged!
-                return self.history
-            self.training_loss = self.training_loss * 0.95 + loss * 0.05 if self.training_loss is not None else loss
+            step_loss = 0
+            for (data, target) in self.train_loader:
+                loss = self.take_step(data, target)
+                step_loss += loss
+                if loss > 10 ** 4:  # Diverged!
+                    step_loss /= len(self.train_loader)
+                    self.training_loss = self.training_loss * 0.95 + step_loss * 0.05 if self.training_loss is not None else step_loss
+                    return self.history
+            self.training_loss = self.training_loss * 0.95 + step_loss * 0.05 if self.training_loss is not None else step_loss
             self.steps += 1
         return self.history
 
