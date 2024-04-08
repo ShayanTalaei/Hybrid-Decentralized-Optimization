78470da43fb38b18dc2be6aab935ccf8e986f5ee
Matin Ansaripour <matinansaripour@gmail.com>
2024-04-08 02:43:45 +0200

    debug

--- optimizers.py
+++ optimizers.py
@@ -104,6 +104,9 @@ class ZAD(Optimizer):
             torch._foreach_mul_(self.grad, self.momentum)
             for _ in range(self.random_vec):
                 v = [torch.randn(p.size()).to(self.device) for p in self.params_data]
+                v_norm = torch._foreach_norm(v)
+                torch._foreach_add_(v_norm, 1e-8)
+                torch._foreach_div_(v, v_norm)
                 params_v = copy.deepcopy(self.params_dict)
                 for p, v_ in zip(params_v.items(), v):
                     p[1].data += v_
