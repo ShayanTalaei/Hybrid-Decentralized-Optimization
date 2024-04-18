8dbcf347359b45ffaf5a4f970736bba7f525e809
Matin Ansaripour <matinansaripour@gmail.com>
2024-04-19 01:42:09 +0200

    debug

--- main.py
+++ main.py
@@ -45,7 +45,6 @@ if __name__ == "__main__":
     parser.add_argument("--out_channels", default=8, type=int, help="The number of output channels for the cnn model.")
     parser.add_argument("--file_name", default=None, help="The name of the file to save the trained model.")
 
-    MPI.Init()
     mpi4py.rc.threads = False
     # MPI.Finalize()
     os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
