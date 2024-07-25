549867edc8b6a8a7d9c041d7d0005ff7698b2c23
Matin <matinansaripour@gmail.com>
2024-07-25 18:10:13 +0200

    Revert "debug"
    This reverts commit 814e0663b2ece4229c43ffb3c2eb99bbf29318b0.

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ def plot_trends(trends, x_axis, y_axis, start=0, path=None, end=float('inf'), da
 
 def run(fn, dataset_name, steps, lr0, lr1, log_period, conv_number=2, hidden=128, num_layer=2, reps=1, path=None,
         file_name=None, model_name=None, freeze_model=False, plot=False, random_vecs=200,
-        num_workers=1, momentum0=0.0, momentum1=0.0, f_grad='first_order', z_grad='zeroth_order_cge', scheduler=False,
+        num_workers=2, momentum0=0.0, momentum1=0.0, f_grad='first_order', z_grad='zeroth_order_cge', scheduler=False,
         scheduler_warmup_steps=0, warmup_steps=0, v_step=10.0, out_channels=8, f_batch_size=100, z_batch_size=100,
         is_cuda_aware=False, device='cpu', config=None):
     results = {}
