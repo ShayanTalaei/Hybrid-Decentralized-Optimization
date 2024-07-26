0b82513894902eb3f541498a5306a9c58c9984b5
Matin <matinansaripour@gmail.com>
2024-07-27 01:44:46 +0200

    .

--- main.py
+++ main.py
@@ -119,6 +119,11 @@ if __name__ == "__main__":
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
 
     # Check if CUDA is available and set the device accordingly
     device = 'cpu'
